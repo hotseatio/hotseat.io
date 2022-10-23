9ef6ce60b3f4f9074a4d32c2c2eed55a3193f071
Nathan Smith <nathan@hotseat.io>
2022-10-23 08:34:47 -0700

    Config test setup

--- jest.config.js
+++ jest.config.js
@@ -125,13 +125,13 @@ module.exports = {
   // roots: [
   //   "<rootDir>"
   // ],
-  roots: ['spec/javascript'],
+  roots: ['test/javascript'],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ['<rootDir>/spec/javascript/globals.js'],
+  setupFiles: ['<rootDir>/test/javascript/globals.js'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
