c3e028b2b60f0533b296e04bd24d85ee4cfdbe24
Nathan Smith <nathan@hotseat.io>
2022-12-19 20:04:16 -1000

    Add a test for quarter_taken

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -4,27 +4,38 @@
 require "test_helper"
 
 class ReviewTest < ActiveSupport::TestCase
-  it "accepts organization ratings only between 1 and 7" do
-    valid_review = build(:review, organization: 5)
-    assert_predicate(valid_review, :valid?)
+  describe "validations" do
+    it "accepts organization ratings only between 1 and 7" do
+      valid_review = build(:review, organization: 5)
+      assert_predicate(valid_review, :valid?)
 
-    invalid_review = build(:review, organization: -11)
-    assert_not(invalid_review.valid?)
+      invalid_review = build(:review, organization: -11)
+      assert_not(invalid_review.valid?)
+    end
+
+    it "accepts clarity ratings only between 1 and 7" do
+      valid_review = build(:review, clarity: 1)
+      assert_predicate(valid_review, :valid?)
+
+      invalid_review = build(:review, clarity: 100)
+      assert_not(invalid_review.valid?)
+    end
+
+    it "accepts overall ratings only between 1 and 7" do
+      valid_review = build(:review, overall: 7)
+      assert_predicate(valid_review, :valid?)
+
+      invalid_review = build(:review, overall: 8)
+      assert_not(invalid_review.valid?)
+    end
   end
 
-  it "accepts clarity ratings only between 1 and 7" do
-    valid_review = build(:review, clarity: 1)
-    assert_predicate(valid_review, :valid?)
-
-    invalid_review = build(:review, clarity: 100)
-    assert_not(invalid_review.valid?)
-  end
-
-  it "accepts overall ratings only between 1 and 7" do
-    valid_review = build(:review, overall: 7)
-    assert_predicate(valid_review, :valid?)
-
-    invalid_review = build(:review, overall: 8)
-    assert_not(invalid_review.valid?)
+  describe "#quarter_taken" do
+    it "returns the quarter that the review is for" do
+      term = T.let(create_current_term, Term)
+      section = create(:section, term:)
+      review = build(:review, section:)
+      assert_nil(review.quarter_taken)
+    end
   end
 end
