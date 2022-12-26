94b32aad11daa31a06ebb63a31dfec2571e45926
Nathan Smith <nathan@hotseat.io>
2022-12-25 23:32:16 -1000

    Create rejected review job

---
+++ app/jobs/notify_user_about_rejected_review_job.rb
@@ -0,0 +1,32 @@
+# typed: strict
+# frozen_string_literal: true
+
+require "aws-sdk-sns"
+
+class NotifyUserAboutRejectedReviewJob < ApplicationJob
+  extend T::Sig
+  queue_as :default
+
+  sig { params(review: Review).void }
+  def perform(review)
+    logger.info("Starting NotifyUserAboutRejectedReviewJob")
+    user = T.must(review.user)
+    section = T.must(review.section)
+
+    if user.phone.nil?
+      logger.warn("User #{user.id} does not have a phone number")
+      return
+    end
+
+    message = <<~MESSAGE
+      Your Hotseat review for #{section.course_title} was not approved. No woriess! You can update and resubmit your review here: #{review.edit_link}
+    MESSAGE
+
+    client = Aws::SNS::Client.new
+    resp = client.publish({
+                            phone_number: user.phone,
+                            message:,
+                          })
+    logger.info("Message sent. Message id: #{resp.message_id}")
+  end
+end
