be1a25d5609ac82eaec265b27f39f7e63444c747
Nathan Smith <nathan@hotseat.io>
2023-02-01 18:37:55 -0800

    wip

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -8,8 +8,37 @@ class Relationship < ApplicationRecord
   belongs_to :section
   has_one :review, dependent: :restrict_with_exception
 
+  #
+  # enum current_status
+
+  class Status < T::Enum
+    enums do
+      Planned = new
+      Subscribed = new
+      Enrolled = new
+
+      Completed = new
+      Reviewed = new
+    end
+  end
+
+  sig { returns(Status) }
+  def status
+    # if review
+    #   Reviewed
+    # elsif section.term in the past
+    #   Completed
+    # else
+    #   current_status
+  end
+
   sig { returns(T::Boolean) }
-  def review?
+  def reviewed?
     review.present?
+  end
+
+  sig { returns(T::Boolean) }
+  def completed?
+    section.term.enrollment_period == Term::EnrollmentPeriod::Post
   end
 end
