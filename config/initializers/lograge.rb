53e6ed543229f6073eaa39e97119705d20bfe044
Nathan Smith <nathan@hotseat.io>
2023-04-21 14:33:39 -0700

    Don't json format logs

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -6,7 +6,6 @@ require "lograge/sql/extension"
 Rails.application.configure do
   # Lograge config
   config.lograge.enabled = true
-  config.lograge.formatter = Lograge::Formatters::Json.new
   config.colorize_logging = false
 
   config.lograge.custom_options = lambda do |event|
