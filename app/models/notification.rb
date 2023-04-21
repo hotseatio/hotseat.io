84a10138b3d8744b83c06a7f6f2f20c56107f6c2
Nathan Smith <nathan@hotseat.io>
2023-04-21 16:42:02 -0700

    rails generate noticed:model

---
+++ app/models/notification.rb
@@ -0,0 +1,7 @@
+# typed: false
+# frozen_string_literal: true
+
+class Notification < ApplicationRecord
+  include Noticed::Model
+  belongs_to :recipient, polymorphic: true
+end
---
+++ test/models/notification_test.rb
@@ -0,0 +1,10 @@
+# typed: false
+# frozen_string_literal: true
+
+require "test_helper"
+
+class NotificationTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
