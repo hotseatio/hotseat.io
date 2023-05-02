9578f7976754493601a076240974dd192ba7cf59
Nathan Smith <nathan@hotseat.io>
2023-05-02 12:15:29 -0700

    force_ssl = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # TODO: make this true when we upgrade Heroku
-  config.force_ssl = false
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
