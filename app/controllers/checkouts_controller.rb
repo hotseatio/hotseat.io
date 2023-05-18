1d6f855235bb5b3677f63b3557d28285fbe8a048
Nathan Smith <nathan@hotseat.io>
2023-05-17 20:33:43 -0700

    Fix logger error

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -25,7 +25,7 @@ class CheckoutsController < ApplicationController
       success_url: my_courses_url(payment_success: true),
       cancel_url: my_courses_url,
     )
-    logger.info("Created checkout session", url: @checkout_session.url)
+    logger.info("Created checkout session url=#{@checkout_session.url}")
 
     redirect_to(@checkout_session.url, allow_other_host: true, status: :see_other)
   end
