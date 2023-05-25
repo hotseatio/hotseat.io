fcff7f07cd0bc2702bd574a497a244adbc224737
Nathan Smith <nathan@hotseat.io>
2023-05-25 08:01:11 -0700

    Tweaks

--- app/mailers/announcements_mailer.rb
+++ app/mailers/announcements_mailer.rb
@@ -10,11 +10,14 @@ class AnnouncementsMailer < ApplicationMailer
     @mailkick_list = T.let("announcements", String)
   end
 
-  # default to: "no-reply@hotseat.io", from: "no-reply@hotseat.io", bcc: -> { User.subscribed("announcements").pluck(:email) }
+  default from: email_address_with_name("announcements@hotseat.io", "Team Hotseat")
 
   sig { void }
   def push_notifications_2023_05_16
     @user = T.let(params[:user], T.nilable(User))
-    mail(to: T.must(@user).email, subject: "Hotseat: Introducing native notifications on all your devices")
+    mail(
+      to: email_address_with_name(T.must(@user).email, T.must(@user).name),
+      subject: "Hotseat: Introducing native notifications on all your devices",
+    )
   end
 end
