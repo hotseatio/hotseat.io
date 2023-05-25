cbfbc1d0ac7227e6912066db9bc0c8a6c447d1f2
Nathan Smith <nathan@hotseat.io>
2023-05-24 21:08:38 -0700

    Get email preview working

--- test/mailers/previews/announcements_mailer_preview.rb
+++ test/mailers/previews/announcements_mailer_preview.rb
@@ -1,5 +1,12 @@
 # typed: strict
+# frozen_string_literal: true
+
 # Preview all emails at http://localhost:3000/rails/mailers/announcements_mailer
 class AnnouncementsMailerPreview < ActionMailer::Preview
+  extend T::Sig
 
+  sig { returns(ActionMailer::MessageDelivery) }
+  def push_notifications_2023_05_16
+    AnnouncementsMailer.push_notifications_2023_05_16
+  end
 end
