abce7539a7b392861b389fa64fddf2503defeddc
Subash R <waytosubash@gmail.com>
2023-06-11 21:37:49 +0530

    Fix Lint error

--- db/migrate/20230611074937_rename_reccomend_textbook_column.rb
+++ db/migrate/20230611074937_rename_reccomend_textbook_column.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class RenameReccomendTextbookColumn < ActiveRecord::Migration[7.0]
   def change
-    rename_column :reviews, :reccomend_textbook, :recommend_textbook
+    rename_column(:reviews, :reccomend_textbook, :recommend_textbook)
   end
 end
