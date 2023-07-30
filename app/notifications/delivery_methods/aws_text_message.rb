fa7febd4e77bee49322c8b9d92bc338975a69724
Nathan Smith <nathan@hotseat.io>
2023-07-29 18:57:12 -0700

    Remove a T.unsafe

--- app/notifications/delivery_methods/aws_text_message.rb
+++ app/notifications/delivery_methods/aws_text_message.rb
@@ -22,7 +22,7 @@ class DeliveryMethods::AwsTextMessage < Noticed::DeliveryMethods::Base
 
     client = Aws::SNS::Client.new(region: "us-east-1")
 
-    if T.unsafe(Rails.env).development?
+    if Rails.env.development?
       Rails.logger.info("Skipping sending message since we're not in production.")
     else
       response = client.publish({ phone_number: recipient.phone, message: notification.full_message })
